98cc09b98b085d3bc84219ad39cf5054d3cb3733
LucasLiaw25 <lucasaliaw@gmail.com>
2025-12-25 15:26:18 -0300

    My first commit

---
+++ Product/src/test/java/com/liaw/dev/Product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaw.dev.Product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
